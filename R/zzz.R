2ab993b637d6cae7d8c4c42b1f96f3d2bed9eb90
Gregory Jefferis <jefferis@gmail.com>
2015-11-13 15:27:31 +0000

    namesace prefix
    * should be optional

--- R/zzz.R
+++ R/zzz.R
@@ -2,6 +2,6 @@
 .onAttach <- function(libname, pkgname) {
   options(rgl.useNULL=TRUE)
   options('flycircuit.scoremat'="allbyallblastcv4.5.ff")
-  fc_attach_bigmat('allbyallblastcv4.5.ff')
+  flycircuit::fc_attach_bigmat('allbyallblastcv4.5.ff')
   invisible()
 }
