ac5eb146424e3dd39e1936687d71e88a8468354d
Gregory Jefferis <jefferis@gmail.com>
2015-12-03 15:26:07 +0000

    switch data load to .onLoad
    * also setting package options

--- R/zzz.R
+++ R/zzz.R
@@ -1,8 +1,12 @@
 #' @importFrom flycircuit load_si_data
-.onAttach <- function(libname, pkgname) {
-  options(rgl.useNULL=TRUE)
+.onLoad <- function(libname, pkgname) {
+  # don't want to use rgl if we are in batch mode
+  if(!interactive())
+    options(rgl.useNULL=TRUE)
+
   options('flycircuit.scoremat'="allbyallblastcv4.5.ff")
   load_si_data('allbyallblastcv4.5.ff')
   load_si_data("scall.sampled.bm.desc")
+
   invisible()
 }
