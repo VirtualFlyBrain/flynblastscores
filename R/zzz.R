48e9d9d2b3578cff707a45164cb4f5a886a9d296
Gregory Jefferis <jefferis@gmail.com>
2020-06-22 12:02:03 +0100

    Wrap .onLoad load_si_data in tryCatch
    * we should still complain if they fail but it may be useful to let the package load

--- R/zzz.R
+++ R/zzz.R
@@ -5,8 +5,14 @@
     options(rgl.useNULL=TRUE)
 
   options('flycircuit.scoremat'="allbyallblastcv4.5.ff")
-  flycircuit::load_si_data('allbyallblastcv4.5.ff')
-  flycircuit::load_si_data("scall.sampled.bm.desc")
+  tryCatch(
+    flycircuit::load_si_data('allbyallblastcv4.5.ff'),
+    error=function(e) warning(e)
+  )
+  tryCatch(
+    flycircuit::load_si_data("scall.sampled.bm.desc"),
+    error=function(e) warning(e)
+  )
 
   invisible()
 }
